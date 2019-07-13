b1c2fbe6e4c380fb00b4974da3f87d2858568a2c
tiedoll95 <52814193+tiedoll95@users.noreply.github.com>
2019-07-13 16:23:28 +0700

    Update startdv.sh

--- startdv.sh
+++ startdv.sh
@@ -1,7 +1,7 @@
-cat <<EOF > /root/startdv.sh
  #!/bin/bash
-if (($(netstat -ntupla | awk '{print $4}' | grep 0.0.0.0:5601 | wc -l) > 0 ))
+if ((($(netstat -ntupla | awk '{print $4}' | grep 0.0.0.0:5601 | wc -l) > 0)&&($(netstat -ntupla | awk '{print $4}' | grep 0.0.0.0:9200 | wc -l) > 0)))
   then
+  echo "elasticsearch and kibana started"
 systemctl start metricbeat
 systemctl start filebeat
 systemctl start packetbeat
@@ -16,6 +16,5 @@ rm -rf /root/elk-stack.sh
 rm -rf /root/elk-stack
 echo "Da thuc hien thanh cong OK"
 else
-  echo "ban thuc hien lai sau 2 phut .kibana chua start"
+  echo "ban thuc hien lai sau 2 phut .kibana or elasticsearch chua start"
 fi
-EOF
